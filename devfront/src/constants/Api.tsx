1ef56986ada69fb94dba6e88cf86997107a70eda
Sangam Mundhe <sangammunde3@gmail.com>
2024-12-27 10:29:16 +0530

    Update Api.tsx

--- devfront/src/constants/Api.tsx
+++ devfront/src/constants/Api.tsx
@@ -1,2 +1,2 @@
-// export const BackendUrl = "https://dev-meet-v1s8.onrender.com";
-export const BackendUrl = "http://localhost:5000";
+export const BackendUrl = "https://dev-meet-v1s8.onrender.com";
+// export const BackendUrl = "http://localhost:5000";
