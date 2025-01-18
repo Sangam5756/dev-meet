5b229b85f1c0e792869a640cb53029ba541393bf
sangam5756 <sangammunde3@gmail.com>
2025-01-18 17:33:16 +0530

    dynamic backend url

--- devfront/src/constants/Api.tsx
+++ devfront/src/constants/Api.tsx
@@ -1,4 +1,4 @@
-// export const BackendUrl = "https://dev-meet123.vercel.app";
+export const BackendUrl = "https://dev-meet123.vercel.app";
 // export const BackendUrl = "http://localhost:5000";
 
-export const BackendUrl = location.hostname === "localhost" ? "http://localhost:5000" :"";
+// export const BackendUrl = location.hostname === "localhost" ? "http://localhost:5000" :"";
