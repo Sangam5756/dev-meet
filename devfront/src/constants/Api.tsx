068283f4514a1452bbaa6cc4aae97883b1c08ee9
sangam5756 <sangammunde3@gmail.com>
2025-01-18 17:19:33 +0530

    domain name changed

--- devfront/src/constants/Api.tsx
+++ devfront/src/constants/Api.tsx
@@ -1,2 +1,2 @@
-// export const BackendUrl = "https://dev-meet123.vercel.app";
-export const BackendUrl = "http://localhost:5000";
+export const BackendUrl = "https://dev-meet123.vercel.app";
+// export const BackendUrl = "http://localhost:5000";
