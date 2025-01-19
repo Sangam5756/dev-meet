d0eda5d9e54445e629fe7288874f5ca97e2e41ec
sangam5756 <sangammunde3@gmail.com>
2025-01-19 14:43:19 +0530

    url changed

--- devfront/src/constants/Api.tsx
+++ devfront/src/constants/Api.tsx
@@ -1,4 +1,5 @@
-export const BackendUrl = "https://dev-meet123.vercel.app";
+// export const BackendUrl = "https://dev-meet123.vercel.app";
+export const BackendUrl = "https://https://devmeetbackend57.onrender.com";
 // export const BackendUrl = "http://localhost:5000";
 
 // export const BackendUrl = location.hostname === "localhost" ? "http://localhost:5000" :"";
